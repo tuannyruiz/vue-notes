a42925e6a349a3e5fbba784f56c9d28920256d62
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-07-28 11:35:39 -0300

    add HeaderBar

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-row :gutter="10" style="display: flex; align-items: center">
+    <el-col :span="20">
+      <h1 style="color: #444">VNotes :)</h1>
+    </el-col>
+    <el-col :span="4">
+      <el-button
+        @click="$emit('add-note')"
+        type="primary"
+        icon="el-icon-plus">Add note</el-button>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'ListNotes'
+}
+</script>
